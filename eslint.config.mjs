b5e7fa0da7f104382571cd9d7bf8a91e3e88fe4b
isoshigi <isoshigicchi@gmail.com>
2026-06-18 14:10:05 +0900

    feat: activate eslint-config-prettire

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import prettierConf from 'eslint-config-prettier'
 
 export default withNuxt(
-  // Your custom configs here
+  prettierConf
 )
