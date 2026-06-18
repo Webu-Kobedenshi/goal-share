bbd65ff7c7f27ebb0f135512aa10509071cbbc5d
isoshigi <isoshigicchi@gmail.com>
2026-06-18 16:34:57 +0900

    fmt: npx prettier -w .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,4 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 import prettierConf from 'eslint-config-prettier'
 
-export default withNuxt(
-  prettierConf
-)
+export default withNuxt(prettierConf)
