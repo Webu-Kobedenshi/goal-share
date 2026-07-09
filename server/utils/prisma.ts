f7d3f56a3fa0629763648dc95490f15bab5aa5ad
isoshigi <isoshigicchi@gmail.com>
2026-07-09 17:57:10 +0900

    fix: update Prisma client initialization and connection pooling

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,4 +1,9 @@
-import { PrismaClient } from '../../app/generated/prisma'
+import { PrismaClient } from '../../prisma/generated/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
 
-const prisma = new PrismaClient()
+const pool = new pg.Pool({ connectionString: process.env.DIRECT_URL })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
+
 export default prisma
